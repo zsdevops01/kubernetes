bcf0f561c8dbeccbc53d97210a3db51bfd795dcc
KalluruS <>
2021-07-02 05:05:03 +0530

    Update kubernetes/

--- create-cluster/approach-1/terraform.tf
+++ create-cluster/approach-1/terraform.tf
@@ -1,28 +1,28 @@
-module "minikube" {
-  source              = "github.com/scholzj/terraform-aws-minikube"
-
-  aws_region          = "us-east-1"
-  cluster_name        = "minikube"
-  aws_instance_type   = "t2.medium"
-  ssh_public_key      = "~/.ssh/id_rsa.pub"
-  aws_subnet_id       = module.vpc.public_subnets[0]
-  ami_image_id        = data.aws_ami.ami.id
-  hosted_zone         = var.HOSTED_ZONE
-  hosted_zone_private = false
-
-  tags = {
-    Application = "Minikube"
-  }
-
-  addons = [
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-  ]
-}
-
-variable "HOSTED_ZONE" {}
+//module "minikube" {
+//  source              = "github.com/scholzj/terraform-aws-minikube"
+//
+//  aws_region          = "us-east-1"
+//  cluster_name        = "minikube"
+//  aws_instance_type   = "t2.medium"
+//  ssh_public_key      = "~/.ssh/id_rsa.pub"
+//  aws_subnet_id       = module.vpc.public_subnets[0]
+//  ami_image_id        = data.aws_ami.ami.id
+//  hosted_zone         = var.HOSTED_ZONE
+//  hosted_zone_private = false
+//
+//  tags = {
+//    Application = "Minikube"
+//  }
+//
+//  addons = [
+//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+//  ]
+//}
+//
+//variable "HOSTED_ZONE" {}
 
 data "aws_ami" "ami" {
   most_recent           = true
