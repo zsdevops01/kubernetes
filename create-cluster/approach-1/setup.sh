f47ad8331be1d1734d3cd36cd3690621831e206f
KalluruS <>
2021-07-02 18:36:08 +0530

    Update kubernetes/

--- create-cluster/approach-1/setup.sh
+++ create-cluster/approach-1/setup.sh
@@ -3,6 +3,10 @@
 read -p 'Enter DNS Hosted Zone Name: ' hname
 export TF_VAR_HOSTED_ZONE=${hname}
 
+if [ ! -f "~/.ssh/id_rsa.pub" ]; then
+  cat /dev/zero | ssh-keygen -q -N ""
+fi
+
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/terraform/install.sh | sudo bash
 terraform init
 terraform apply -auto-approve
