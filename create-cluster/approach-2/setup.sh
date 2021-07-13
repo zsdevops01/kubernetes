01d1a88f5e49656e34216603222470755aaf4555
KalluruS <>
2021-07-13 18:21:21 +0530

    Update kubernetes/

--- create-cluster/approach-2/setup.sh
+++ create-cluster/approach-2/setup.sh
@@ -5,5 +5,5 @@ curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/
 curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/k8-client-stack/install.sh | sudo bash
 curl https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/eksctl/install.sh | sudo bash
 
-eksctl create cluster --name sample --region us-east-1 --managed
+eksctl create cluster --name sample --region us-east-1 --managed --zones us-east-1a,us-east-1b
 
